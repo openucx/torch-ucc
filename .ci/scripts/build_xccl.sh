d78acd421ebbd1176ec3ba1ed4c7cf175d7d67c4
artemry-nv <artemry@nvidia.com>
2021-03-29 22:42:15 +0300

    Fixed merge issues
    Signed-off-by: artemry-nv <artemry@nvidia.com>

--- .ci/scripts/build_xccl.sh
+++ .ci/scripts/build_xccl.sh
@@ -10,7 +10,7 @@ cd "${XCCL_SRC_DIR}/build-${XCCL_BUILD_TYPE}"
 # TODO enable CUDA (compilation failed)
 #"${XCCL_SRC_DIR}/configure" --with-ucx="${UCX_INSTALL_DIR}" --prefix="${XCCL_INSTALL_DIR}" --enable-debug
 "${XCCL_SRC_DIR}/configure" --with-cuda="${CUDA_HOME}" --with-ucx="${UCX_INSTALL_DIR}" \
---prefix="${XCCL_INSTALL_DIR}" --enable-debug
+    --prefix="${XCCL_INSTALL_DIR}" --enable-debug
 make -j install
 echo "${XCCL_INSTALL_DIR}/lib" > /etc/ld.so.conf.d/xccl.conf
 ldconfig
--- .ci/scripts/install_torch.sh
+++ .ci/scripts/install_torch.sh
@@ -1,16 +1,40 @@
 #!/bin/bash -eEx
+set -o pipefail
 
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
+# TODO debug
+#cd /tmp
+#git clone https://github.com/pytorch/pytorch.git
+#cd /tmp/pytorch
+#git submodule sync --recursive
+#git submodule update --init --recursive
+#pip3 install -r requirements.txt
+#export TORCH_CUDA_ARCH_LIST="7.0 8.0+PTX"
+#export USE_GLOO=1
+#export USE_DISTRIBUTED=1
+#export USE_OPENCV=0
+## TODO debug
+#export USE_CUDA=1
+##export USE_CUDA=0
+#export USE_NCCL=0
+#export USE_MKLDNN=0
+#export BUILD_TEST=0
+#export USE_FBGEMM=0
+#export USE_NNPACK=0
+#export USE_QNNPACK=0
+#export USE_XNNPACK=0
+#export USE_KINETO=1
+#export MAX_JOBS=$(($(nproc)-1))
+#python setup.py install
+#cd -
+#rm -rf /tmp/pytorch
 
-# shellcheck disable=SC2034
-#DLRM_MODEL="big"
-DLRM_MODEL="small"
+# TODO debug
+#conda install -y pytorch torchvision cpuonly -c pytorch-nightly
+#conda install pytorch torchvision torchaudio cudatoolkit=11.0 -c pytorch-nightly
+#conda uninstall -y pytorch torchvision
+#conda install pytorch torchvision cudatoolkit=11.0 -c pytorch-nightly
+#conda install pytorch cudatoolkit=11.0 -c pytorch-nightly
 
-HOSTNAME=$(hostname -s)
-export HOSTNAME
-SRC_ROOT_DIR=$(cd "${SCRIPT_DIR}/../../" && pwd -P)
-export CONFIGS_DIR="${SRC_ROOT_DIR}/.ci/configs"
-
-# DLRM MASTER_PORT
-export MASTER_PORT="12346"
-export DOCKER_SSH_PORT="12345"
+pip3 install --default-timeout=900 numpy
+pip3 install --default-timeout=900 --pre torch -f https://download.pytorch.org/whl/nightly/cu110/torch_nightly.html
+pip3 install "git+https://github.com/mlperf/logging.git@0.7.1"
